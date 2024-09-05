68cbfec9a86a6af6795a8f34ea6295de103c53c8
Hernan Febri <hernanfebri@gmail.com>
2024-09-05 13:48:57 +0700

    Update Halaman Login

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  LoginView({super.key});
+
+  final TextEditingController emailC = TextEditingController();
+  final TextEditingController passC = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LoginView'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            autocorrect: false,
+            controller: emailC,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "Email",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Obx(
+            () => TextField(
+              autocorrect: false,
+              controller: passC,
+              keyboardType: TextInputType.text,
+              obscureText: controller.isHidden.value,
+              decoration: InputDecoration(
+                labelText: "Password",
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      controller.isHidden.toggle();
+                    },
+                    icon: Icon(controller.isHidden.isFalse
+                        ? Icons.remove_red_eye
+                        : Icons.remove_red_eye_outlined)),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(9),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 20),
+            ),
+            child: const Text("LOGIN"),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,20 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const home = _Paths.home;
+  static const login = _Paths.login;
+  static const addProduct = _Paths.addProduct;
+  static const product = _Paths.product;
+  static const detailProduct = _Paths.detailProduct;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const home = '/home';
+  static const login = '/login';
+  static const addProduct = '/add-product';
+  static const product = '/product';
+  static const detailProduct = '/detail-product';
+}
