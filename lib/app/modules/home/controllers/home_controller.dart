ebd8878520afa6bde28fa7bc044f41a01336a2a0
Hernan Febri <hernanfebri@gmail.com>
2024-09-26 14:34:31 +0700

    Fix Catalog Load Database

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,25 +1,188 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
-import 'package:open_file_plus/open_file_plus.dart'; // Ganti dengan open_file_plus
+import 'package:open_file_plus/open_file_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:qrcode_app/app/data/models/product_model.dart';
 
 class HomeController extends GetxController {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  RxList<ProductModel> allProducts = List<ProductModel>.empty().obs;
+
   void downloadCatalog() async {
     final pdf = pw.Document();
 
+    var getData = await firestore.collection("products").get();
+
+    // reset all products -> untuk mengatasi duplikat
+    allProducts([]);
+
+    // Isi Data allProducts dari database
+    for (var element in getData.docs) {
+      allProducts.add(ProductModel.fromJson(element.data()));
+    }
+
     pdf.addPage(
-      pw.Page(
+      pw.MultiPage(
         pageFormat: PdfPageFormat.a4,
-        build: (context) => pw.Center(
-          child: pw.Text(
-            "Test pdf",
-            style: const pw.TextStyle(fontSize: 50),
-          ),
-        ),
+        build: (context) {
+          List<pw.TableRow> allData = List.generate(
+            allProducts.length,
+            (index) {
+              ProductModel product = allProducts[index];
+              return pw.TableRow(
+                children: [
+                  // Nomor
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      "${index + 1}",
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  // Product Code
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      product.code,
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  // Product Name
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      product.name,
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  // Qty
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      "${product.qty}",
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  // QR Code
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.BarcodeWidget(
+                      data: product.code,
+                      barcode: pw.Barcode.qrCode(),
+                      color: PdfColor.fromHex("#000000"),
+                      height: 50,
+                      width: 50,
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+
+          return [
+            pw.Center(
+              child: pw.Text(
+                "CATALOG PRODUCTS",
+                textAlign: pw.TextAlign.center,
+                style: const pw.TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            pw.SizedBox(height: 20),
+            pw.Table(
+              border: pw.TableBorder.all(
+                color: PdfColor.fromHex("#000000"),
+                width: 2,
+              ),
+              children: [
+                pw.TableRow(
+                  children: [
+                    // Nomor
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "No",
+                        textAlign: pw.TextAlign.center,
+                        style: const pw.TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    ),
+                    // Product Code
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "Product Code",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // Product Name
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "Product Name",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // Qty
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "Quantity",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // QR Code
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "QR Code",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // ISI DATANYA
+                ...allData,
+              ],
+            )
+          ];
+        },
       ),
     );
 
@@ -34,7 +197,6 @@ class HomeController extends GetxController {
     await file.writeAsBytes(bytes);
 
     // open pdf menggunakan open_file_plus
-    final result = await OpenFile.open(file.path);
-    print("Result: ${result.message}");
+    await OpenFile.open(file.path);
   }
 }
